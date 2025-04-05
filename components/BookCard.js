63c1918569cd7eb287ee0099436098739a436c84
Richard Chien <stdrc@outlook.com>
2025-04-05 23:40:33 +0800

    Add book title
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- components/BookCard.js
+++ components/BookCard.js
@@ -4,54 +4,80 @@ export default function BookCard({ book }) {
   return (
     <div
       style={{
-        border: "1px solid #eaeaea",
-        boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-        height: "200px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.8rem",
         width: "135px",
-        overflow: "hidden",
       }}
     >
-      {book.coverUrl ? (
-        <img
-          src={`https://wsrv.nl/?url=${book.coverUrl}&w=135&h=200`}
-          alt={book.name}
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-          }}
-        />
-      ) : (
-        <div
-          style={{
-            padding: "2rem 1rem",
-            display: "flex",
-            flexDirection: "column",
-            gap: "0.4rem",
-            overflow: "hidden",
-            alignItems: "flex-end",
-            textAlign: "end",
-          }}
-        >
+      <div
+        style={{
+          border: "1px solid #eaeaea",
+          boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+          height: "200px",
+          width: "100%",
+          overflow: "hidden",
+          display: "flex",
+        }}
+      >
+        {book.coverUrl ? (
+          <img
+            src={`https://wsrv.nl/?url=${book.coverUrl}&w=135&h=200`}
+            alt={book.name}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        ) : (
           <div
             style={{
-              fontSize: "16px",
-              fontWeight: "bold",
+              width: "100%",
+              padding: "2rem 1rem",
+              display: "flex",
+              flexDirection: "column",
+              gap: "0.4rem",
+              overflow: "hidden",
+              alignItems: "flex-end",
+              textAlign: "end",
             }}
           >
-            {book.name}
-          </div>
+            <div
+              style={{
+                fontSize: "15px",
+                fontWeight: "bold",
+              }}
+            >
+              {book.name}
+            </div>
 
-          <div
-            style={{
-              marginTop: 0,
-              fontSize: "13px",
-            }}
-          >
-            {book.authors.join(", ")}
+            <div
+              style={{
+                marginTop: 0,
+                fontSize: "13px",
+              }}
+            >
+              {book.authors.join(", ")}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          fontSize: "15px",
+          fontWeight: "bold",
+          // textAlign: "center",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+        title={book.name}
+      >
+        {book.name}
+      </div>
     </div>
   );
 }
