0dbe6b70a84349aaf778ea23002f96e6e3a7532a
Richard Chien <stdrc@outlook.com>
2025-04-06 02:08:25 +0800

    将BookCard组件内联样式提取到CSS Module文件

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,81 +1,26 @@
 import { formatDate } from "../lib/notion-books";
+import styles from "./BookCard.module.css";
 
 export default function BookCard({ book }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "0.8rem",
-        width: "135px",
-        fontFamily: "var(--chinese-sans-font-family)",
-      }}
-    >
-      <div
-        style={{
-          border: "1px solid #eaeaea",
-          boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-          height: "200px",
-          width: "100%",
-          overflow: "hidden",
-          display: "flex",
-        }}
-      >
+    <div className={styles.bookCard}>
+      <div className={styles.imageContainer}>
         {book.coverUrl ? (
           <img
             src={`https://wsrv.nl/?url=${book.coverUrl}&w=135&h=200`}
             alt={book.name}
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
+            className={styles.coverImage}
           />
         ) : (
-          <div
-            style={{
-              width: "100%",
-              padding: "2rem 1rem",
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.25rem",
-              overflow: "hidden",
-              alignItems: "flex-end",
-              textAlign: "end",
-            }}
-          >
-            <div
-              style={{
-                fontSize: "15px",
-                fontWeight: "bold",
-              }}
-            >
-              {book.name}
-            </div>
+          <div className={styles.fallbackCover}>
+            <div className={styles.bookTitle}>{book.name}</div>
 
-            <div
-              style={{
-                marginTop: 0,
-                fontSize: "13px",
-              }}
-            >
-              {book.authors.join(", ")}
-            </div>
+            <div className={styles.bookAuthors}>{book.authors.join(", ")}</div>
           </div>
         )}
       </div>
 
-      <div
-        style={{
-          width: "100%",
-          fontSize: "15px",
-          fontWeight: "bold",
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-        }}
-        title={book.name}
-      >
+      <div className={styles.bookCardTitle} title={book.name}>
         {book.name}
       </div>
     </div>
